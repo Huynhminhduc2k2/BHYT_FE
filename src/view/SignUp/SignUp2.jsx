2c8ad301a8b6bb79e8a66065fb2a5cea10762092
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2023-12-03 00:12:54 +0700

    Finish styling for SignUp pages, update styling of SignIn page

--- src/view/SignUp/SignUp2.jsx
+++ src/view/SignUp/SignUp2.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
+import SignUpLogo from '../../assets/logos/HealthInsurance.png';
 
 function SignUp2() {
   return (
     <>
-      <section>
-        <div>
-          <form action="/SignUp2" method="post">
-            <h1>Register</h1>
+      <section className="SignUp">
+        <div className="SignUp-Container">
+          <form action="/SignUp2" method="post" className="SignUp-Form">
+            <h1 className="SignUp-Form-Title">Register</h1>
 
             <input
               type="text"
-              placeholder="Enter Email / Phone Number"
-              name="email_phone"
+              placeholder="Enter Code"
+              name="code"
+              className="code-Input"
             />
-            <p>Code is valid for 2 minutes.</p>
-            <p>Resend code after 1 minute</p>
-            <button type="submit">
+
+            <div className="SignUp-Form-Outlines">
+              <p>Code is valid for 2 minutes.</p>
+              <p>Resend code after 1 minute</p>
+            </div>
+
+            <button type="submit" className="verify-Btn">
               <b>Verify</b>
             </button>
           </form>
         </div>
 
-        <img src="" alt="" />
+        <img src={SignUpLogo} alt="sign up logo" className="SignUp-Logo" />
       </section>
     </>
   );
