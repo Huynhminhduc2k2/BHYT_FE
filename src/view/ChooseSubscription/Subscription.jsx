35537fc8c93906af109cf55460b7c20e8ce03be6
nhokluss <nhok8rytaband@gmail.com>
2023-12-23 12:58:39 +0700

    payment

--- src/view/ChooseSubscription/Subscription.jsx
+++ src/view/ChooseSubscription/Subscription.jsx
@@ -1,37 +1,40 @@
 import React, { useState } from 'react';
-import "./Subscription.css"
+import "./Subscription.css" 
 import { Button } from '@chakra-ui/react';
 const Subscription = () => {
   return (
     <div className="pricing-plans">
       <div className="plan">
         <h2>Basic</h2>
-        <p className="price">$10<span>/month</span></p>
+        <p className="price">A$5.99<span>/month</span></p>
         <ul>
           <li>Feature 1</li>
           <li>Feature 2</li>
           <li>Feature 3</li>
         </ul>
     
-        <Button colorScheme='blue'>Subscribe</Button>
+        <form action="https://localhost:7067/v2/api/Payment/Basic" method="POST">
+    <Button type="submit" colorScheme='green'>Subbmit</Button>
+    </form>
       </div>
 
       <div className="plan popular">
-        <span className="popular-badge">Popular</span>
-        <h2>Pro</h2>
-        <p className="price">$40<span>/6 months</span></p>
+        <h2>Advance</h2>
+        <p className="price">A$6.99<span>/6 months</span></p>
         <ul>
           <li>Feature 1</li>
           <li>Feature 2</li>
           <li>Feature 3</li>
           <li>Feature 4</li>
         </ul>
-        <Button colorScheme='blue'>Subscribe</Button>
+        <form action="https://localhost:7067/v2/api/Payment/Advance" method="POST">
+    <Button type="submit" colorScheme='green'>Subbmit</Button>
+    </form>
       </div>
 
       <div className="plan">
         <h2>Premium</h2>
-        <p className="price">$100<span>/year</span></p>
+        <p className="price">A$9.99<span>/year</span></p>
         <ul>
           <li>Feature 1</li>
           <li>Feature 2</li>
@@ -39,7 +42,9 @@ const Subscription = () => {
           <li>Feature 4</li>
           <li>Feature 5</li>
         </ul>
-        <Button colorScheme='blue'>Subscribe</Button>
+        <form action="https://localhost:7067/v2/api/Payment/Premium" method="POST">
+    <Button type="submit" colorScheme='green'>Subbmit</Button>
+    </form>
       </div>
     </div>
   );
