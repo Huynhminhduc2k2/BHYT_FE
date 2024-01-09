1f70842ff52ba39b20888db0369be015d1ab0436
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2024-01-09 19:17:34 +0700

    Hard cord admin page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { userState, useState } from 'react';
+
 import './App.css';
 import SignIn from './view/SignIn/SignIn';
 import SignUp1 from './view/SignUp/SignUp1';
 import SignUp2 from './view/SignUp/SignUp2';
 import MainPage from './view/MainPage/MainPage';
-import { userState, useState } from 'react';
 import InsuranceRegistrationForm from './view/InsuranceRegisForm/RegisForm';
 import Subscription from './view/ChooseSubscription/Subscription';
 import Payment from './view/Payment/Payment';
 import UserProfile from './view/Profile/Profile';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import {
   BrowserRouter as Router,
@@ -16,20 +18,20 @@ import {
   Routes,
   Navigate,
 } from 'react-router-dom';
-import { ReactDOM } from 'react';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/home" element={<MainPage />}  />
+          <Route path="/home" element={<MainPage />} />
           <Route path="/" element={<SignIn />} default />
-          <Route path="/signup" element={<SignUp1 />}  />
-          <Route path="/signupVerify" element={<SignUp2 />}  />
+          <Route path="/signup" element={<SignUp1 />} />
+          <Route path="/signupVerify" element={<SignUp2 />} />
           <Route path="/insuregis" element={<InsuranceRegistrationForm />} />
           <Route path="/subscription" element={<Subscription />} />
           <Route path="/payment" element={<Payment />} />
+          <Route path="/profile" element={<UserProfile />} />
           <Route path="/profile" element={<UserProfile />} />
         </Routes>
       </Router>
--- src/view/MainPage/MainPage.jsx
+++ src/view/MainPage/MainPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MainPage.css';
+import HeaderMain from './components/HeaderMain.jsx';
+import ContentMain from './components/ContentMain.jsx';
 
 function MainPage() {
   return (
-    <>
-      <h1>hello world from MainPage</h1>
-    </>
+    <div>
+        <HeaderMain />
+        <ContentMain />
+    </div>
   );
 }
 
